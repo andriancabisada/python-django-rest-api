6cf1a4ed27a791242b358304bb6d08d10235d001
Andrian Cabisada <82685346+andriancabisada@users.noreply.github.com>
2023-08-23 08:03:53 +0800

    Initial commit

---
+++ myapp/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
