2a67ebda905c8ebe1a39b642eed48d12b3c1ef60
Andrian Cabisada <82685346+andriancabisada@users.noreply.github.com>
2023-08-24 11:33:35 +0800

    added test cases

--- myapp/tests.py
+++ myapp/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from myapp.models import Product, Category
 
-# Create your tests here.
+class CategoryTestCase(TestCase):
+    def setUp(self):
+        self.category = Category.objects.create(name="Electronics")
+
+    def test_category_name(self):
+        self.assertEqual(self.category.name, "Electronics")
+
+class ProductTestCase(TestCase):
+    def setUp(self):
+        # Create a category
+        self.category = Category.objects.create(name="Electronics")
+
+        # Create a product associated with the category
+        self.product = Product.objects.create(
+            name="Smartphone",
+            description="A modern smartphone",
+            category=self.category,
+        )
+
+    def test_product_name(self):
+        self.assertEqual(self.product.name, "Smartphone")
+
+    def test_product_description(self):
+        self.assertEqual(self.product.description, "A modern smartphone")
+
+    def test_product_category(self):
+        self.assertEqual(self.product.category, self.category)
+
+    def test_product_category_name(self):
+        # Test accessing the category name through the product
+        self.assertEqual(self.product.category.name, "Electronics")
